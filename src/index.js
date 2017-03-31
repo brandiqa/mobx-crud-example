79195f5f5c0b8b74d40feff2fb04791eec04529a
Michael Wanyoike <micwanyoike@gmail.com>
2017-03-31 13:53:22 +0300

    Installed React Router Dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
