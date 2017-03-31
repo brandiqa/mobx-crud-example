ef27dd6fc0ff1fb743f6a989a9ec10126e465bdd
Michael Wanyoike <micwanyoike@gmail.com>
2017-03-31 17:26:42 +0300

    minor cleanup

--- src/pages/contact-list-page.js
+++ src/pages/contact-list-page.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import store from '../stores/contact-store'
 import ContactList from '../components/contact-list';
-// import { contacts } from '../mock-data';
 
 class ContactListPage extends Component {
   render() {
