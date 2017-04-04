8aa1e5b435fdd24db174d409aa3262fc420e0dff
Michael Wanyoike <micwanyoike@gmail.com>
2017-04-04 21:07:51 +0300

    defined phone number validation on server

--- backend/src/services/contact/contact-model.js
+++ backend/src/services/contact/contact-model.js
@@ -15,7 +15,16 @@ const contactSchema = new Schema({
     last: { type: String, required: false }
   },
   email : { type: mongoose.SchemaTypes.Email, required: [true, "Email is required"] },
-  phone : { type: String, required: [true, "Phone is required"] },
+  phone : {
+    type: String,
+    required: [true, "Phone is required"],
+    validate: {
+      validator: function(v){
+        return /^\+(?:[0-9] ?){6,14}[0-9]$/.test(v);
+      },
+      message: "{VALUE} is not a valid international phone number"
+    }
+  },
   createdAt: { type: Date, 'default': Date.now },
   updatedAt: { type: Date, 'default': Date.now }
 });
