8cf1f7f5ae673f794eaa3f9140b1994816a6ce15
Michael Wanyoike <micwanyoike@gmail.com>
2017-04-10 16:53:22 +0300

    Refactored contact-form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,21 +1,78 @@
-import React from 'react'
+import React from 'react';
 import { observer } from 'mobx-react';
-import { Form, Button } from 'semantic-ui-react';
+import MobxReactForm from 'mobx-react-form';
+import MobxReactFormDevTools from 'mobx-react-form-devtools';
+import validatorjs from 'validatorjs';
+import { Form, Button, Grid } from 'semantic-ui-react';
 import { Redirect } from 'react-router';
 import InputField from './input-field';
+import store from '../stores/contact-store';
+
+const fields = {
+  name:{
+    name: 'name',
+    label: 'Name',
+    fields: [{
+      name: 'first',
+      label: 'First Name',
+      placeholder: 'First Name',
+      rules: 'required|string'
+    },
+    {
+      name: 'last',
+      label: 'Last Name',
+      placeholder: 'Last Name',
+      rules: 'string'
+    }]
+  },
+  phone: {
+    name: 'phone',
+    label: 'Phone',
+    placeholder: 'Enter phone number',
+    rules: 'required|string'
+  },
+  email: {
+    name: 'email',
+    label: 'Email',
+    placeholder: 'Enter email address',
+    rules: 'required|string|email'
+  }
+};
+
+class MobxForm extends MobxReactForm {
+  onSuccess(form) {
+    const contact = Object.assign({}, store.contact, form.values());
+    if(contact._id) {
+      store.updateContact(contact)
+    } else {
+      store.saveContact(contact);
+    }
+  }
+}
 
 @observer
-class ContactForm  extends React.Component {
+class ContactForm extends React.Component {
+
+  form = null;
+
+  componentWillMount() {
+    const plugins = { dvr: validatorjs };
+    this.form = new MobxForm({fields},{plugins});
+    MobxReactFormDevTools.register({
+      contactForm: this.form
+    });
+    MobxReactFormDevTools.select('contactForm');
+  }
 
   componentWillReceiveProps = (nextProps) => {
-    const {contact} = nextProps.store;
-    const { form } = this.props;
-    form.update(contact)
+    const contact = nextProps.contact;
+    this.form.update(contact);
   }
 
   render() {
-    const { form } = this.props;
-    const { redirect, loading, errors } = this.props.store
+    const form = this.form;
+    const { redirect, loading, errors } = store;
+
     const formComponent = (
       <Form onSubmit={form.onSubmit} loading={loading}>
         <Form.Group widths='equal'>
@@ -26,13 +83,25 @@ class ContactForm  extends React.Component {
         <InputField field={form.$('email')} error={errors.email} />
         <Button primary type='submit' onClick={form.onSubmit} disabled={form.isPristine}>Save</Button>
       </Form>
-    )
+    );
+
+    const grid = (
+      <div>
+        <MobxReactFormDevTools.UI />
+        <Grid centered columns={2}>
+          <Grid.Column>
+            <h1 style={{marginTop:"1em"}}>Add New Contact</h1>
+            {formComponent}
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
 
     return (
       <div>
-        { redirect ? <Redirect to="/" /> : formComponent }
+        { redirect ? <Redirect to="/" /> : grid }
       </div>
-    )
+    );
   }
 }
 
--- src/pages/contact-form-page.js
+++ src/pages/contact-form-page.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react';
-import { Grid} from 'semantic-ui-react';
-import validatorjs from 'validatorjs';
-import MobxReactFormDevTools from 'mobx-react-form-devtools';
 import ContactForm from '../components/contact-form';
-import Form, {fields} from '../forms/contact';
 import store from '../stores/contact-store';
 import { observer } from 'mobx-react';
 
 @observer
 class ContactFormPage extends Component {
-
-  form = null;
-
-  componentWillMount() {
-    const plugins = { dvr: validatorjs };
-    this.form = new Form({fields},{plugins});
-    MobxReactFormDevTools.register({
-      contactForm: this.form
-    });
-    MobxReactFormDevTools.select('contactForm');
-  }
 
   componentDidMount() {
     const { _id } = this.props.match.params;
@@ -33,13 +18,7 @@ class ContactFormPage extends Component {
   render() {
     return (
       <div>
-        <MobxReactFormDevTools.UI />
-        <Grid centered columns={2}>
-          <Grid.Column>
-            <h1 style={{marginTop:"1em"}}>Add New Contact</h1>
-            <ContactForm form={this.form} store={store} contact={store.contact}/>
-          </Grid.Column>
-        </Grid>
+        <ContactForm contact={store.contact} />
       </div>
     )
   }
