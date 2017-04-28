1c45a310af99e85ac4608f82cf31ac48fbddfa7b
Michael Wanyoike <micwanyoike@gmail.com>
2017-04-28 16:31:26 +0300

    removed unused error var

--- src/components/input-field.js
+++ src/components/input-field.js
@@ -3,13 +3,12 @@ import { observer } from 'mobx-react';
 import { Form } from 'semantic-ui-react';
 import classnames from 'classnames';
 
-export default observer(({field, error }) => (
-  <Form.Field className={classnames({error:field.error || error })}>
+export default observer(({field}) => (
+  <Form.Field className={classnames({error:field.error})}>
     <label htmlFor={field.id}>
       {field.label}
     </label>
     <input {...field.bind()} />
     <span className="error">{field.error}</span>
-    <span className="error">{error}</span>
   </Form.Field>
 ));
